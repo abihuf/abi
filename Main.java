2e0c6e7095ef9e0130926aa2e80e31df1a378ced
Polly <tt08547@gmail.com>
2024-09-28 18:11:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,24 @@
+import java.io.PrintStream;
 import java.util.Scanner;
-//ПОКА РАБОТАЕТ ДЛЯ ПОЛОЖИТЕЛЬНЫХ
+//ПОКА РАБОТАЕТ ДЛЯ НЕОТРИЦАТЕЛЬНЫХ
 public class Main {
+    public static PrintStream out = System.out;
+    public static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         //подаем число на ввод
-        Scanner in = new Scanner(System.in);
         System.out.println("Введите число:");
         int num = in.nextInt();
         //пустая переменная типа данных String, с помощью которой "переворачиваем" число
-        String total = "";
+        String res = "";
         //это для нолика
         if (num == 0) {
             System.out.println(0);
         }
-        while (num > 0) {
-            // находим остаток
-            int bin = num % 2;
-            //конвертируем в String
-            String bin2 = String.valueOf(bin);
-            total = bin2 + total;
+        while (num != 0) {
+            res = (num % 2) + res;
             num = num / 2;
         }
         //тадам!
-        System.out.println(total);
+        System.out.println(res);
     }
 }
